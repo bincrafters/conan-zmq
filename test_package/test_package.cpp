7acfbc422d560e6f308173e606d11fba77a514a7
SSE4 <tomskside@gmail.com>
2018-02-18 13:23:06 +0700

    - switch to CMake build

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,7 +1,9 @@
 #include <zmq.h>
+#include <cstdlib>
+#include <iostream>
 #include <stdexcept>
 
-int main()
+int main() try
 {
     void *context = zmq_ctx_new();
     void *requester = zmq_socket(context, ZMQ_REQ);
@@ -12,4 +14,10 @@ int main()
 #endif
     zmq_close(requester);
     zmq_ctx_destroy (context);
+
+    return EXIT_SUCCESS;
+}
+catch (std::runtime_error & e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
 }
